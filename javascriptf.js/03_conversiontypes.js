0d307be7125041abbde9e39cd59a1a2486910066
Sonu Raj <srsonurj@gmail.com>
2026-01-22 17:44:33 +0000

    conversion types

--- javascriptf.js/03_conversiontypes.js
+++ javascriptf.js/03_conversiontypes.js
@@ -1,2 +1,26 @@
-let naam= "sonuraj"
-console.log(typeof naam); //string
+let score= "44"
+let scoreValue = Boolean(score)
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 ; false => 0
+//null => 0
+//undefined => NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+//1=> true; 0=> false
+//"" => false; 
+// "Sonu" => true
+
+let somenumber= 38
+let numberToString = String(somenumber)
+console.log(numberToString);
+console.log(typeof numberToString);
+
