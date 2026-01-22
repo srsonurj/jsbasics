fc3f28395c355dc62230528513df3b70b7fc6a6f
Sonu Raj <srsonurj@gmail.com>
2026-01-22 04:20:13 +0000

    datat

--- javascriptf.js/02_datatypes.js
+++ javascriptf.js/02_datatypes.js
@@ -1,4 +1,12 @@
-const accountId = 579567575
-let accountCity= "Patna"
-let accountLoggedIn= true
-console.table([accountId,accountCity,accountLoggedIn]);
+let name = "Sonu"
+let age = 19
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
