422e7db4e2d9d6e5abb8b5a53d02f04df58df0d4
Sonu Raj <srsonurj@gmail.com>
2026-01-31 13:24:22 +0000

    notes_on_string_included

--- javascriptf.js/05_Stringbasics.js
+++ javascriptf.js/05_Stringbasics.js
@@ -3,4 +3,18 @@ const repco = 77
 // console.log(`my name is ${myname} and my repo count is ${repco}`);
 
 const NewString = new String("hardik")
-console.log(NewString.__proto__); //object
+console.log(NewString.length);
+
+
+
+// ***************** NOTES on String Basics *****************
+/* There are many methods in string prototype which we can use on strings
+1. length => gives length of string
+2. toUpperCase() => converts string to uppercase
+3. toLowerCase() => converts string to lowercase
+4. slice(startIndex, endIndex) => extracts part of string from startIndex to endIndex-1
+5. replace(oldSubstr, newSubstr) => replaces oldSubstr with newSubstr in the string
+6. concat(str1, str2, ...) => concatenates multiple strings
+7. includes(substr) => checks if substr is present in the string, returns true/false
+
+   etc.......*/
