6c6418749fefc7f6f7e8b9ca003c229fb71da6cb
Sonu Raj <srsonurj@gmail.com>
2026-01-31 11:11:56 +0000

    stringbasics

---
+++ javascriptf.js/05_Stringbasics.js
@@ -0,0 +1,6 @@
+const myname = "Sonu"
+const repco = 77
+// console.log(`my name is ${myname} and my repo count is ${repco}`);
+
+const NewString = new String("hardik")
+console.log(NewString.__proto__); //object
