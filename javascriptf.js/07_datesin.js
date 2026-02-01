69897a581751858b46cf849ace359644e452e88a
Sonu Raj <srsonurj@gmail.com>
2026-02-01 18:18:54 +0000

    datesbasics

---
+++ javascriptf.js/07_datesin.js
@@ -0,0 +1,12 @@
+// Dates
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.toString());
+// console.log(newDate.toDateString());
+// console.log(newDate.toLocaleDateString());
+
+let myDate = new Date()
+// console.log(myDate.toLocaleString());
+
+
