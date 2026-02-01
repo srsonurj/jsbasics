78b36a07086153a1dc59bee5b1e25252a7c54cce
Sonu Raj <srsonurj@gmail.com>
2026-02-01 07:52:20 +0000

    nums_and_maths

---
+++ javascriptf.js/06_nums_maths.js
@@ -0,0 +1,30 @@
+const goal = 4
+// console.log(goal);
+
+const balance = new Number (250)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const num = 24.675
+// console.log(num.toPrecision(3));
+
+const num2 = 1000000000
+// console.log(num2.toLocaleString('en-IN'))
+
+
+// +++++++++++++++++++++ Maths +++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.ceil(35.1));
+// console.log(Math.max(3, 7, 56 , 45));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+
+
+const min = 20
+const max = 50
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
