87a3fdf0ebe5896f72aab8964e82cfd9b5a3153b
Sonu Raj <srsonurj@gmail.com>
2026-02-02 07:28:40 +0000

    updated_date_time

--- javascriptf.js/07_datesin.js
+++ javascriptf.js/07_datesin.js
@@ -10,3 +10,30 @@ let myDate = new Date()
 // console.log(myDate.toLocaleString());
 
 
+let dates = new Date (2026, 01, 02)
+// console.log(dates);
+
+let myCreatedDate = new Date("2026-02-02")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate2 = new Date()
+// console.log(newDate2);
+// console.log(newDate2.getMonth() + 1) ;
+// console.log(newDate2.getDay()) ;
+// console.log(newDate2.getFullYear());
+
+console.log(newDate2.toLocaleString('default', {
+
+    weekday: 'long'
+}));
+
+
+
+
+
+
